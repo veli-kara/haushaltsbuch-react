58ab72c47fadc18f5acf39782520392ef626fccd
velikarade <veli.kara@docc.techstarter.de>
2024-03-21 16:19:44 +0100

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
-import { Link } from 'react-router-dom';
-
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+//import Form from 'react-bootstrap/Form';
 
 
+function NavbarCreator () {
+    return (
+        <>
+            <Navbar className="justify-content-center" style={{ backgroundColor: 'lightblue' }}>
+                <Container>
+                <Navbar.Brand>Haushaltsbuch</Navbar.Brand>
+                <Nav>
+                    <Nav.Link href='#Einkommen'><Button variant="outline-secondary">Einkommen</Button>{' '}</Nav.Link>
+                    <Nav.Link href='#Ausgabe'><Button variant="outline-primary">Ausgaben</Button>{' '}</Nav.Link>
+                    <Nav.Link href='#Zusammenfassung'><Button variant="outline-danger">Zusammenfassung</Button>{' '}</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>        
+        </>
+    )
+}
+
+export default NavbarCreator;
